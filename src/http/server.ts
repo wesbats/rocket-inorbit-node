777ce480af6a9bbc09ee3a525c72df0d0003c380
Weslley <weslleyvitorbatista@gmail.com>
2024-09-11 11:23:05 -0300

    created "goals" route, post method

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,32 @@
 import fastify from 'fastify'
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+import createGoal from '../services/create-goal'
+import z from 'zod';
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post('/goals',{
+  schema: {
+    body: z.object({
+      title: z.string(),
+      desiredWeeklyFrequency: z.number().int().min(1).max(7)
+    })
+  }
+} , async (request)=> {
+  const {title, desiredWeeklyFrequency} = request.body
+
+  await createGoal({
+    title,
+    desiredWeeklyFrequency,
+  })
+})
 
 app
   .listen({
-    port: 333,
+    port: 3333,
   })
   .then(() => {
     console.log('HTTP server running!')
