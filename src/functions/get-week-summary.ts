8da9a380dba54aa96944478281fc54fdc321ab0d
Weslley <weslleyvitorbatista@gmail.com>
2024-09-12 15:13:23 -0300

    created type GoalsPerDay for response

--- src/functions/get-week-summary.ts
+++ src/functions/get-week-summary.ts
@@ -57,6 +57,15 @@ export async function getWeekSummary() {
       .groupBy(goalscompletedInWeek.completedAtDate)
   )
 
+  type GoalsPerDay = Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+
   const result = await db
     .with(goalsCreatedUpToWeek, goalscompletedInWeek, goalsCompletedByWeekDay)
     .select({
@@ -68,7 +77,7 @@ export async function getWeekSummary() {
         sql /*sql*/`(SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek})`.mapWith(
           Number
         ),
-      goalsPerDay: sql /*sql*/`
+      goalsPerDay: sql /*sql*/<GoalsPerDay>`
           JSON_OBJECT_AGG(
             ${goalsCompletedByWeekDay.completedAtDate}, ${goalsCompletedByWeekDay.completions}
           )
@@ -77,6 +86,6 @@ export async function getWeekSummary() {
     .from(goalsCompletedByWeekDay)
 
   return {
-    summary: result,
+    summary: result[0],
   }
 }
